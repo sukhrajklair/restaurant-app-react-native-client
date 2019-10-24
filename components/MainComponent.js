414af66d220faadc30a1954dca5f8a47a84401d8
Sukhraj Klair <sukhrajklair@gmail.com>
2019-10-24 08:58:42 -0700

    Simple Menu list with onPress handler

---
+++ components/MainComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import {DISHES} from '../shared/dishes';
+import Dishdetail from './DishdetailComponent';
+import { View } from 'react-native';
+
+class Main extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        }
+    }
+    onDishSelect(dishId){
+        this.setState({selectedDish:dishId});
+    }
+    render(){
+        return (
+            <View > 
+                <Menu dishes = {this.state.dishes} 
+                    onPress = {dishId=> this.onDishSelect(dishId)}/>
+                <Dishdetail 
+                    dish = {this.state.dishes.filter(
+                        dish=> dish.id === this.state.selectedDish
+                    )}
+                />
+            </View>
+        )
+    }
+}
+
+export default Main;
